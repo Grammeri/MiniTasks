bc8d3e0efda65f5287e469dd5d886874533c899d
Grammeri <reacttsxgrammer@gmail.com>
2022-03-25 11:17:47 -0500

    Initial state

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,13 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-import {FullInput} from "./components/FullInput";
-import {Input} from "./components/Input";
-import {Button} from "./components/Button";
+
 
 function App() {
-  let [message, setMessage]=useState([
-    {message:"message-1"},
-    {message:"message-2"},
-    {message:"message-3"},
-  ])
-
-  let [title, setTitle]=useState("")
-  console.log(title)
-
-  const addMessage = (title:string) => {
-  let newMessage = {message:title}
-    setMessage([newMessage,...message])
-  }
-
-  const callBackBtnHandler = ()=> {
-addMessage(title)
-    setTitle("")
-  }
 
   return (
     <div className="App">
-      <Input title={title}
-      setTitle={setTitle}/>
-      <Button name={"+"} callBack={callBackBtnHandler}/>
 
-      {message.map((el, index)=>{
-      return(
-          <div key={index}>{el.message}</div>
-      )
-      })}
     </div>
   );
 }
